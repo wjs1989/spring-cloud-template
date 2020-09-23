6a5804c0ef82a16b0aa8d498f2ea098d83515465
wjs1989 <wenjs001@163.com>
2020-09-23 18:29:36 +0800

    redis

---
+++ popular-common-parent/resource-common-parent/ehcache-auxiliary/src/main/java/com/wjs/ehcache/EhcacheAuxiliaryAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.wjs.ehcache;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configurable
+@ComponentScan
+public class EhcacheAuxiliaryAutoConfiguration {
+
+}
---
+++ popular-common-parent/resource-common-parent/redis-auxiliary/src/main/java/com/wjs/redis/RedisAuxiliaryAutoConfiguration.java
@@ -0,0 +1,11 @@
+package com.wjs.redis;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configurable
+@ComponentScan
+public class RedisAuxiliaryAutoConfiguration {
+
+
+}
