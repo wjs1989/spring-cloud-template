84b1b812e52a334f4a7a483392a120028d6e64da
wjs1989 <wenjs001@163.com>
2021-03-10 18:57:49 +0800

    1111

--- doc/AAA/src/main.js
+++ doc/AAA/src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 // import permission from './permission.js'
 import router from './router'
-
+import '@/element-ui'                         // api: https://github.com/ElemeFE/element
+import '@/icons'                              // api: http://www.iconfont.cn/
+import '@/element-ui-theme'
 
 Vue.config.productionTip = false
 
--- doc/AAA/src/views/LatestNews/index.vue
+++ doc/AAA/src/views/LatestNews/index.vue
@@ -1,14 +1,49 @@
 <template>
 	<!-- 最新资讯 -->
-	<div class="content" >
-		<WebHeader v-bind:msg="message"/>  
-		
-		<h3>最新资讯</h3>
+	<div class="content">
+		<WebHeader v-bind:msg="message" />
+
+		<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+			<el-menu-item index="1">处理中心</el-menu-item>
+			<el-submenu index="2">
+				<template slot="title">我的工作台</template>
+				<el-menu-item index="2-1">选项1</el-menu-item>
+				<el-menu-item index="2-2">选项2</el-menu-item>
+				<el-menu-item index="2-3">选项3</el-menu-item>
+				<el-submenu index="2-4">
+					<template slot="title">选项4</template>
+					<el-menu-item index="2-4-1">选项1</el-menu-item>
+					<el-menu-item index="2-4-2">选项2</el-menu-item>
+					<el-menu-item index="2-4-3">选项3</el-menu-item>
+				</el-submenu>
+			</el-submenu>
+			<el-menu-item index="3" disabled>消息中心</el-menu-item>
+			<el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+		</el-menu>
+		<div class="line"></div>
+		<el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect"
+		 background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+			<el-menu-item index="1">处理中心</el-menu-item>
+			<el-submenu index="2">
+				<template slot="title">我的工作台</template>
+				<el-menu-item index="2-1">选项1</el-menu-item>
+				<el-menu-item index="2-2">选项2</el-menu-item>
+				<el-menu-item index="2-3">选项3</el-menu-item>
+				<el-submenu index="2-4">
+					<template slot="title">选项4</template>
+					<el-menu-item index="2-4-1">选项1</el-menu-item>
+					<el-menu-item index="2-4-2">选项2</el-menu-item>
+					<el-menu-item index="2-4-3">选项3</el-menu-item>
+				</el-submenu>
+			</el-submenu>
+			<el-menu-item index="3" disabled>消息中心</el-menu-item>
+			<el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+		</el-menu>
 		<Footer></Footer>
-	</div> 
-	
-	
-	
+	</div>
+
+
+
 </template>
 
 
@@ -20,8 +55,8 @@
 		name: "latestNews",
 		data() {
 			return {
-				// 版本号
-				version: "3.3.0",
+				activeIndex: '1',
+				activeIndex2: '1'
 			};
 		},
 		components: {
@@ -29,15 +64,15 @@
 			Footer
 		},
 		methods: {
-			goTarget(href) {
-				window.open(href, "_blank");
-			},
+			handleSelect(key, keyPath) {
+				console.log(key, keyPath);
+			}
 		},
 	};
 </script>
 
 <style>
- .content {
-   min-height: 100%;
- }
+	.content {
+		min-height: 100%;
+	}
 </style>
