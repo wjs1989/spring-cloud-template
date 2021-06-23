219e971b3d56c3002a44071d5a3e4004075155b1
wjs1989 <wenjs001@163.com>
2021-06-23 15:43:09 +0800

    kafka

--- module-template/module-kafka/src/main/java/com/wjs/kafka/service/KafkaSender.java
+++ module-template/module-kafka/src/main/java/com/wjs/kafka/service/KafkaSender.java
@@ -1,25 +1,79 @@
 package com.wjs.kafka.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
-/**
- * @author King老师   享学课堂 https://enjoy.ke.qq.com
- * 往期课程咨询芊芊老师  QQ：2130753077 VIP课程咨询 依娜老师  QQ：2133576719
- * 类说明：
- */
+import java.text.MessageFormat;
+
 @Component
 public class KafkaSender {
 
-    @Autowired
-    private KafkaTemplate<String,String> kafkaTemplate;
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public void messageSender(String tpoic,String key,String message){
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(String topic, String message) {
+        send(topic, null, message);
+    }
+
+    public void send(String topic, String key, String message) {
         try {
-            kafkaTemplate.send(tpoic,key,message);
+            ListenableFuture<SendResult<String, String>> send = kafkaTemplate.send(topic, key, message);
+            send.addCallback(new DefaultListenableFutureCallback(topic, key, message));
+
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error(MessageFormat.format("消息发送异常，topic={0},key={1},message={2}", topic, key, message), e);
+        }
+    }
+
+    /**
+     * 指定发送分区
+     *
+     * @param topic
+     * @param partition
+     * @param key
+     * @param message
+     */
+    public void send(String topic, Integer partition, String key, String message) {
+        try {
+            ListenableFuture<SendResult<String, String>> send = kafkaTemplate.send(topic, partition, key, message);
+            send.addCallback(new DefaultListenableFutureCallback(topic, key, message));
+        } catch (Exception e) {
+            logger.error(MessageFormat.format("消息发送异常，topic={0},key={1},message={2}", topic, key, message), e);
+        }
+    }
+
+    /**
+     * 发送结果监听
+     */
+    public class DefaultListenableFutureCallback implements ListenableFutureCallback {
+        private String topic;
+        private String key;
+        private String message;
+
+        public DefaultListenableFutureCallback(String topic, String key, String message) {
+            this.topic = topic;
+            this.key = key;
+            this.message = message;
+        }
+
+        @Override
+        public void onSuccess(Object o) {
+            if (logger.isDebugEnabled()) {
+                logger.debug(MessageFormat.format("消息发送成功:{0}", o.toString()));
+            }
+        }
+
+        @Override
+        public void onFailure(Throwable t) {
+            logger.error(MessageFormat.format("消息发送失败，topic={0},key={1},message={2}", topic, key, message), t);
         }
     }
 }
